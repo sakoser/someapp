65d1643180985dbb4e010c80ae4f808a84267898
sakoser <150455636+sakoser@users.noreply.github.com>
2023-11-30 00:26:29 +0500

    Update TipsApp.py

--- TipsApp.py
+++ TipsApp.py
@@ -1,8 +1,8 @@
+import matplotlib
 import streamlit as st
 import pandas as pd
 import pandas as pd
 import numpy as np
-import matplotlib as plt
 import matplotlib.pyplot as plt 
 import seaborn as sns
 from datetime import datetime, timedelta
